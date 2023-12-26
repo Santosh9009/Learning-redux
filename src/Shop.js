937821b8b865279a3c8aa6df0dab3af21ef469ae
Santosh9009 <patisantosh00@gmail.com>
2023-12-26 16:30:55 +0530

    first

---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <button type="button">Add</button>
+      <button type="button">Remove</button>
+    </div>
+  )
+}
+
+export default Shop
